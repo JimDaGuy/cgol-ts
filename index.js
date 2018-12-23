666ed86bb9dfb24875e6b9d2e03ec58c6522b17a
James Digrazia <jtd2401@rit.edu>
2018-12-22 21:55:08 -0500

    Add christmas setup and cell clearing buttons

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 {
     // Canvas Dimensions
-    const canvasWidth = 1000;
-    const canvasHeight = 500;
-    const squareWidth = 25;
-    const lineWidth = 2;
-    const numHorCells = canvasWidth / squareWidth;
-    const numVertCells = canvasHeight / squareWidth;
-    const lineColor = 'black';
-    const deadColor = 'green';
-    const aliveColor = 'red';
-    const canvas = document.createElement('canvas');
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
-    canvas.style.clear = 'both';
-    canvas.style.margin = '20px';
-    document.body.appendChild(canvas);
-    const ctx = canvas.getContext('2d');
+    var canvasWidth_1 = 1000;
+    var canvasHeight_1 = 500;
+    var squareWidth_1 = 25;
+    var lineWidth_1 = 2;
+    var numHorCells_1 = canvasWidth_1 / squareWidth_1;
+    var numVertCells_1 = canvasHeight_1 / squareWidth_1;
+    var lineColor_1 = 'black';
+    var deadColor_1 = 'green';
+    var aliveColor_1 = 'red';
+    var h1 = document.createElement('h1');
+    h1.style.textAlign = 'center';
+    h1.style.margin = '20px 0';
+    h1.innerHTML = 'Conway\'s Game of Life - <a href="https://github.com/JimDaGuy/cgol-ts">JimDaGuy</a>';
+    document.body.append(h1);
+    var canvas_1 = document.createElement('canvas');
+    canvas_1.width = canvasWidth_1;
+    canvas_1.height = canvasHeight_1;
+    canvas_1.style.clear = 'both';
+    canvas_1.style.margin = "20px calc(50% - " + canvasWidth_1 / 2 + "px)";
+    document.body.append(canvas_1);
+    var ctx_1 = canvas_1.getContext('2d');
     /*
       Rules
       ==========================
@@ -24,13 +29,11 @@
       Any live cell with more than three live neighbors dies, as if by overpopulation.
       Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
     */
-    let aliveCells = {};
-    let newAliveCells = {};
     // Create an empty object and copy the aliveCells values to it
-    const createNewAlive = (currentAlive) => {
-        let newAlive = {};
-        for (let x in currentAlive) {
-            for (let y in currentAlive[x]) {
+    var createNewAlive_1 = function (currentAlive) {
+        var newAlive = {};
+        for (var x in currentAlive) {
+            for (var y in currentAlive[x]) {
                 if (x in newAlive) {
                     newAlive[x][y] = true;
                 }
@@ -42,69 +45,130 @@
         }
         return newAlive;
     };
+    // Custom setup
+    var christmasSetup_1 = {};
+    christmasSetup_1['17'] = {};
+    christmasSetup_1['17']['12'] = true;
+    christmasSetup_1['17']['13'] = true;
+    christmasSetup_1['18'] = {};
+    christmasSetup_1['18']['10'] = true;
+    christmasSetup_1['18']['11'] = true;
+    christmasSetup_1['18']['12'] = true;
+    christmasSetup_1['18']['13'] = true;
+    christmasSetup_1['19'] = {};
+    christmasSetup_1['19']['8'] = true;
+    christmasSetup_1['19']['9'] = true;
+    christmasSetup_1['19']['10'] = true;
+    christmasSetup_1['19']['11'] = true;
+    christmasSetup_1['19']['12'] = true;
+    christmasSetup_1['19']['13'] = true;
+    christmasSetup_1['20'] = {};
+    christmasSetup_1['20']['7'] = true;
+    christmasSetup_1['20']['8'] = true;
+    christmasSetup_1['20']['9'] = true;
+    christmasSetup_1['20']['10'] = true;
+    christmasSetup_1['20']['11'] = true;
+    christmasSetup_1['20']['12'] = true;
+    christmasSetup_1['20']['13'] = true;
+    christmasSetup_1['21'] = {};
+    christmasSetup_1['21']['6'] = true;
+    christmasSetup_1['21']['7'] = true;
+    christmasSetup_1['21']['8'] = true;
+    christmasSetup_1['21']['9'] = true;
+    christmasSetup_1['21']['10'] = true;
+    christmasSetup_1['21']['11'] = true;
+    christmasSetup_1['21']['12'] = true;
+    christmasSetup_1['21']['13'] = true;
+    christmasSetup_1['21']['14'] = true;
+    christmasSetup_1['21']['15'] = true;
+    christmasSetup_1['22'] = {};
+    christmasSetup_1['22']['7'] = true;
+    christmasSetup_1['22']['8'] = true;
+    christmasSetup_1['22']['9'] = true;
+    christmasSetup_1['22']['10'] = true;
+    christmasSetup_1['22']['11'] = true;
+    christmasSetup_1['22']['12'] = true;
+    christmasSetup_1['22']['13'] = true;
+    christmasSetup_1['23'] = {};
+    christmasSetup_1['23']['8'] = true;
+    christmasSetup_1['23']['9'] = true;
+    christmasSetup_1['23']['10'] = true;
+    christmasSetup_1['23']['11'] = true;
+    christmasSetup_1['23']['12'] = true;
+    christmasSetup_1['23']['13'] = true;
+    christmasSetup_1['24'] = {};
+    christmasSetup_1['24']['10'] = true;
+    christmasSetup_1['24']['11'] = true;
+    christmasSetup_1['24']['12'] = true;
+    christmasSetup_1['24']['13'] = true;
+    christmasSetup_1['25'] = {};
+    christmasSetup_1['25']['12'] = true;
+    christmasSetup_1['25']['13'] = true;
+    var aliveCells_1 = createNewAlive_1(christmasSetup_1);
+    var newAliveCells_1 = {};
     // Draw lines on the board
-    const drawLines = () => {
-        ctx.strokeStyle = lineColor;
-        ctx.lineWidth = lineWidth;
-        let lineX = 0;
-        for (let i = lineX; i <= canvasWidth; i += squareWidth) {
-            ctx.beginPath();
-            ctx.moveTo(i, 0);
-            ctx.lineTo(i, canvasHeight);
-            ctx.stroke();
+    var drawLines_1 = function () {
+        ctx_1.strokeStyle = lineColor_1;
+        ctx_1.lineWidth = lineWidth_1;
+        var lineX = 0;
+        for (var i = lineX; i <= canvasWidth_1; i += squareWidth_1) {
+            ctx_1.beginPath();
+            ctx_1.moveTo(i, 0);
+            ctx_1.lineTo(i, canvasHeight_1);
+            ctx_1.stroke();
         }
-        let lineY = 0;
-        for (let i = lineY; i <= canvasWidth; i += squareWidth) {
-            ctx.beginPath();
-            ctx.moveTo(0, i);
-            ctx.lineTo(canvasWidth, i);
-            ctx.stroke();
+        var lineY = 0;
+        for (var i = lineY; i <= canvasWidth_1; i += squareWidth_1) {
+            ctx_1.beginPath();
+            ctx_1.moveTo(0, i);
+            ctx_1.lineTo(canvasWidth_1, i);
+            ctx_1.stroke();
         }
     };
     // Iterate through alive cells and draw them
-    const drawCells = () => {
-        ctx.fillStyle = deadColor;
-        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-        ctx.fillStyle = aliveColor;
-        let xCells = Object.keys(aliveCells);
-        for (let i = 0; i < xCells.length; i++) {
-            let currXCell = xCells[i];
-            let yCells = Object.keys(aliveCells[currXCell]);
-            for (let j = 0; j < yCells.length; j++) {
-                let currYCell = yCells[j];
-                let x = parseInt(currXCell) - 1;
-                let y = parseInt(currYCell) - 1;
-                ctx.fillRect(x * squareWidth, y * squareWidth, squareWidth, squareWidth);
+    var drawCells_1 = function () {
+        ctx_1.fillStyle = deadColor_1;
+        ctx_1.fillRect(0, 0, canvasWidth_1, canvasHeight_1);
+        ctx_1.fillStyle = aliveColor_1;
+        var xCells = Object.keys(aliveCells_1);
+        for (var i = 0; i < xCells.length; i++) {
+            var currXCell = xCells[i];
+            var yCells = Object.keys(aliveCells_1[currXCell]);
+            for (var j = 0; j < yCells.length; j++) {
+                var currYCell = yCells[j];
+                var x = parseInt(currXCell) - 1;
+                var y = parseInt(currYCell) - 1;
+                ctx_1.fillRect(x * squareWidth_1, y * squareWidth_1, squareWidth_1, squareWidth_1);
             }
         }
     };
     // Update the life of cells after a cycle
-    const updateCells = () => {
+    var updateCells_1 = function () {
         // Create copy of currently alive cells
-        newAliveCells = createNewAlive(aliveCells);
+        newAliveCells_1 = createNewAlive_1(aliveCells_1);
         // Check every cell
-        for (let i = 1; i <= numHorCells; i++) {
-            for (let j = 1; j <= numVertCells; j++) {
+        for (var i = 1; i <= numHorCells_1; i++) {
+            for (var j = 1; j <= numVertCells_1; j++) {
                 // Check if current cell is alive
-                let alive = false;
-                let x = i.toString();
-                let y = j.toString();
-                if (x in aliveCells) {
-                    if (y in aliveCells[x]) {
+                var alive = false;
+                var x = i.toString();
+                var y = j.toString();
+                if (x in aliveCells_1) {
+                    if (y in aliveCells_1[x]) {
                         alive = true;
                     }
                 }
                 // Check number of alive neighbors
-                let aliveNeighbors = 0;
+                var aliveNeighbors = 0;
                 // Iterate through neighbor positions, ignoring the cell being checked
-                for (let k = -1; k < 2; k++) {
-                    for (let l = -1; l < 2; l++) {
+                for (var k = -1; k < 2; k++) {
+                    for (var l = -1; l < 2; l++) {
                         if (k != 0 || l != 0) {
-                            let neighborX = (i + k).toString();
-                            let neighborY = (j + l).toString();
+                            var neighborX = (i + k).toString();
+                            var neighborY = (j + l).toString();
                             // Check if current neighbor is alive
-                            if (neighborX in aliveCells) {
-                                if (neighborY in aliveCells[neighborX]) {
+                            if (neighborX in aliveCells_1) {
+                                if (neighborY in aliveCells_1[neighborX]) {
                                     aliveNeighbors++;
                                 }
                             }
@@ -114,55 +178,86 @@
                 // Any live cell with fewer than two live neighbors dies, as if by underpopulation. 
                 // Any live cell with more than three live neighbors dies, as if by overpopulation.
                 if (alive && (aliveNeighbors < 2 || aliveNeighbors > 3)) {
-                    delete newAliveCells[x][y];
+                    delete newAliveCells_1[x][y];
                 }
                 // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
                 else if (!alive && aliveNeighbors === 3) {
-                    if (x in newAliveCells) {
-                        newAliveCells[x][y] = true;
+                    if (x in newAliveCells_1) {
+                        newAliveCells_1[x][y] = true;
                     }
                     else {
-                        newAliveCells[x] = {};
-                        newAliveCells[x][y] = true;
+                        newAliveCells_1[x] = {};
+                        newAliveCells_1[x][y] = true;
                     }
                 }
             }
         }
-        aliveCells = newAliveCells;
-        drawCells();
-        drawLines();
+        aliveCells_1 = newAliveCells_1;
+        drawCells_1();
+        drawLines_1();
     };
     // Set a cell to alive when the canvas is clicked
-    const canvasClick = (e) => {
-        let rect = canvas.getBoundingClientRect();
-        let x = Math.ceil((e.clientX - rect.left) / squareWidth).toString();
-        let y = Math.ceil((e.clientY - rect.top) / squareWidth).toString();
+    var canvasClick_1 = function (e) {
+        var rect = canvas_1.getBoundingClientRect();
+        var x = Math.ceil((e.clientX - rect.left) / squareWidth_1).toString();
+        var y = Math.ceil((e.clientY - rect.top) / squareWidth_1).toString();
         // Add clicked cell to alive cells
-        if (x in aliveCells) {
-            aliveCells[x][y] = true;
+        if (x in aliveCells_1) {
+            aliveCells_1[x][y] = true;
         }
         else {
-            aliveCells[x] = {};
-            aliveCells[x][y] = true;
+            aliveCells_1[x] = {};
+            aliveCells_1[x][y] = true;
         }
         // Redraw cells and lines
-        drawCells();
-        drawLines();
+        drawCells_1();
+        drawLines_1();
     };
-    const init = () => {
-        drawCells();
-        drawLines();
-        canvas.onclick = canvasClick;
-        let br = document.createElement('br');
-        document.body.append(br);
-        let button = document.createElement('button');
-        button.innerText = 'Update Cells';
-        button.style.width = '200px';
-        button.style.height = '100px';
-        button.style.cssFloat = 'left';
-        button.onclick = updateCells;
-        button.style.backgroundColor = 'green';
-        document.body.append(button);
+    var init = function () {
+        drawCells_1();
+        drawLines_1();
+        canvas_1.onclick = canvasClick_1;
+        var updateButton = document.createElement('button');
+        updateButton.innerText = 'Update Cells';
+        updateButton.style.width = '200px';
+        updateButton.style.height = '75px';
+        updateButton.style.cssFloat = 'left';
+        updateButton.style.fontSize = "1.5em";
+        updateButton.style.backgroundColor = 'black';
+        updateButton.style.color = 'white';
+        updateButton.style.margin = "20px 20px 20px calc(50% - " + canvasWidth_1 / 2 + "px)";
+        updateButton.onclick = updateCells_1;
+        document.body.append(updateButton);
+        var clearButton = document.createElement('button');
+        clearButton.innerText = 'Clear Cells';
+        clearButton.style.width = '200px';
+        clearButton.style.height = '75px';
+        clearButton.style.cssFloat = 'left';
+        clearButton.style.fontSize = "1.5em";
+        clearButton.style.backgroundColor = 'black';
+        clearButton.style.color = 'white';
+        clearButton.style.margin = '20px';
+        clearButton.onclick = function () {
+            aliveCells_1 = {};
+            drawCells_1();
+            drawLines_1();
+        };
+        document.body.append(clearButton);
+        var christmasButton = document.createElement('button');
+        christmasButton.innerText = 'Christmas Cells';
+        christmasButton.style.width = '200px';
+        christmasButton.style.height = '75px';
+        christmasButton.style.cssFloat = 'left';
+        christmasButton.style.fontSize = "1.5em";
+        christmasButton.style.backgroundColor = 'black';
+        christmasButton.style.color = 'white';
+        christmasButton.style.margin = '20px';
+        christmasButton.onclick = function () {
+            aliveCells_1 = createNewAlive_1(christmasSetup_1);
+            drawCells_1();
+            drawLines_1();
+        };
+        document.body.append(christmasButton);
     };
     init();
 }
--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@
   const canvasHeight: number = 500;
   const squareWidth: number = 25;
   const lineWidth: number = 2;
-  
+
   const numHorCells: number = canvasWidth / squareWidth;
   const numVertCells: number = canvasHeight / squareWidth;
 
@@ -12,12 +12,18 @@
   const deadColor: string = 'green';
   const aliveColor: string = 'red';
 
+  const h1 = document.createElement('h1');
+  h1.style.textAlign = 'center';
+  h1.style.margin = '20px 0';
+  h1.innerHTML = 'Conway\'s Game of Life - <a href="https://github.com/JimDaGuy/cgol-ts">JimDaGuy</a>';
+  document.body.append(h1);
+
   const canvas = document.createElement('canvas');
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
   canvas.style.clear = 'both';
-  canvas.style.margin = '20px';
-  document.body.appendChild(canvas);
+  canvas.style.margin = `20px calc(50% - ${canvasWidth / 2}px)`;
+  document.body.append(canvas);
   const ctx = canvas.getContext('2d');
 
   /*
@@ -29,16 +35,13 @@
     Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
   */
 
-  let aliveCells = {};
-  let newAliveCells = {};
-
   // Create an empty object and copy the aliveCells values to it
   const createNewAlive = (currentAlive) => {
     let newAlive = {};
     for (let x in currentAlive) {
       for (let y in currentAlive[x]) {
         if (x in newAlive) {
-          newAlive[x][y] = true;          
+          newAlive[x][y] = true;
         } else {
           newAlive[x] = {};
           newAlive[x][y] = true;
@@ -47,6 +50,69 @@
     }
     return newAlive;
   };
+
+  // Custom setup
+  let christmasSetup = {};
+  christmasSetup['17'] = {};
+  christmasSetup['17']['12'] = true;
+  christmasSetup['17']['13'] = true;
+  christmasSetup['18'] = {};
+  christmasSetup['18']['10'] = true;
+  christmasSetup['18']['11'] = true;
+  christmasSetup['18']['12'] = true;
+  christmasSetup['18']['13'] = true;
+  christmasSetup['19'] = {};
+  christmasSetup['19']['8'] = true;
+  christmasSetup['19']['9'] = true;
+  christmasSetup['19']['10'] = true;
+  christmasSetup['19']['11'] = true;
+  christmasSetup['19']['12'] = true;
+  christmasSetup['19']['13'] = true;
+  christmasSetup['20'] = {};
+  christmasSetup['20']['7'] = true;
+  christmasSetup['20']['8'] = true;
+  christmasSetup['20']['9'] = true;
+  christmasSetup['20']['10'] = true;
+  christmasSetup['20']['11'] = true;
+  christmasSetup['20']['12'] = true;
+  christmasSetup['20']['13'] = true;
+  christmasSetup['21'] = {};
+  christmasSetup['21']['6'] = true;
+  christmasSetup['21']['7'] = true;
+  christmasSetup['21']['8'] = true;
+  christmasSetup['21']['9'] = true;
+  christmasSetup['21']['10'] = true;
+  christmasSetup['21']['11'] = true;
+  christmasSetup['21']['12'] = true;
+  christmasSetup['21']['13'] = true;
+  christmasSetup['21']['14'] = true;
+  christmasSetup['21']['15'] = true;
+  christmasSetup['22'] = {};
+  christmasSetup['22']['7'] = true;
+  christmasSetup['22']['8'] = true;
+  christmasSetup['22']['9'] = true;
+  christmasSetup['22']['10'] = true;
+  christmasSetup['22']['11'] = true;
+  christmasSetup['22']['12'] = true;
+  christmasSetup['22']['13'] = true;
+  christmasSetup['23'] = {};
+  christmasSetup['23']['8'] = true;
+  christmasSetup['23']['9'] = true;
+  christmasSetup['23']['10'] = true;
+  christmasSetup['23']['11'] = true;
+  christmasSetup['23']['12'] = true;
+  christmasSetup['23']['13'] = true;
+  christmasSetup['24'] = {};
+  christmasSetup['24']['10'] = true;
+  christmasSetup['24']['11'] = true;
+  christmasSetup['24']['12'] = true;
+  christmasSetup['24']['13'] = true;
+  christmasSetup['25'] = {};
+  christmasSetup['25']['12'] = true;
+  christmasSetup['25']['13'] = true;
+
+  let aliveCells = createNewAlive(christmasSetup);
+  let newAliveCells = {};
 
   // Draw lines on the board
   const drawLines = () => {
@@ -73,14 +139,14 @@
   // Iterate through alive cells and draw them
   const drawCells = () => {
     ctx.fillStyle = deadColor;
-    ctx.fillRect( 0, 0, canvasWidth, canvasHeight);
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
     ctx.fillStyle = aliveColor;
     let xCells = Object.keys(aliveCells);
     for (let i = 0; i < xCells.length; i++) {
       let currXCell = xCells[i];
       let yCells = Object.keys(aliveCells[currXCell]);
-      for (let j = 0; j < yCells.length; j++) { 
+      for (let j = 0; j < yCells.length; j++) {
         let currYCell = yCells[j];
         let x = parseInt(currXCell) - 1;
         let y = parseInt(currYCell) - 1;
@@ -130,7 +196,7 @@
         // Any live cell with more than three live neighbors dies, as if by overpopulation.
         if (alive && (aliveNeighbors < 2 || aliveNeighbors > 3)) {
           delete newAliveCells[x][y];
-        } 
+        }
         // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
         else if (!alive && aliveNeighbors === 3) {
           if (x in newAliveCells) {
@@ -142,7 +208,7 @@
         }
       }
     }
-    
+
     aliveCells = newAliveCells;
     drawCells();
     drawLines();
@@ -151,7 +217,7 @@
   // Set a cell to alive when the canvas is clicked
   const canvasClick = (e) => {
     let rect = canvas.getBoundingClientRect();
-    let x: string = Math.ceil(( e.clientX - rect.left) / squareWidth).toString();
+    let x: string = Math.ceil((e.clientX - rect.left) / squareWidth).toString();
     let y: string = Math.ceil((e.clientY - rect.top) / squareWidth).toString();
 
     // Add clicked cell to alive cells
@@ -173,17 +239,49 @@
 
     canvas.onclick = canvasClick;
 
-    let br = document.createElement('br');
-    document.body.append(br);
+    let updateButton = document.createElement('button');
+    updateButton.innerText = 'Update Cells';
+    updateButton.style.width = '200px';
+    updateButton.style.height = '75px';
+    updateButton.style.cssFloat = 'left';
+    updateButton.style.fontSize = "1.5em";
+    updateButton.style.backgroundColor = 'black';
+    updateButton.style.color = 'white';
+    updateButton.style.margin = `20px 20px 20px calc(50% - ${canvasWidth / 2}px)`;
+    updateButton.onclick = updateCells;
+    document.body.append(updateButton);
 
-    let button = document.createElement('button');
-    button.innerText = 'Update Cells';
-    button.style.width = '200px';
-    button.style.height = '100px';
-    button.style.cssFloat = 'left';
-    button.onclick = updateCells;
-    button.style.backgroundColor = 'green';
-    document.body.append(button);
+    let clearButton = document.createElement('button');
+    clearButton.innerText = 'Clear Cells';
+    clearButton.style.width = '200px';
+    clearButton.style.height = '75px';
+    clearButton.style.cssFloat = 'left';
+    clearButton.style.fontSize = "1.5em";
+    clearButton.style.backgroundColor = 'black';
+    clearButton.style.color = 'white';
+    clearButton.style.margin = '20px';
+    clearButton.onclick = () => {
+      aliveCells = {};
+      drawCells();
+      drawLines();
+    };
+    document.body.append(clearButton);
+
+    let christmasButton = document.createElement('button');
+    christmasButton.innerText = 'Christmas Cells';
+    christmasButton.style.width = '200px';
+    christmasButton.style.height = '75px';
+    christmasButton.style.cssFloat = 'left';
+    christmasButton.style.fontSize = "1.5em";
+    christmasButton.style.backgroundColor = 'black';
+    christmasButton.style.color = 'white';
+    christmasButton.style.margin = '20px';
+    christmasButton.onclick = () => {
+      aliveCells = createNewAlive(christmasSetup);
+      drawCells();
+      drawLines();
+    };
+    document.body.append(christmasButton);
   };
 
   init();
